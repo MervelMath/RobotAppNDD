5fb47ac68442850186ec61c068d43d46f600380e
Matheus F. Lopes <matheusdefigueiredo@gmail.com>
2021-04-11 13:15:56 -0300

    Commit 1

---
+++ source/repos/RoboApp/RoboApp.ConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Console.WriteLine("Tamanho de X:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tamanho de Y:");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Direção inicial:");
+            string d = Console.ReadLine();
+
+            Console.WriteLine("Posição de X:");
+            int e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Posição de Y:");
+            int f = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entrada:");
+            string a = Console.ReadLine();
+            */
+
+            Console.WriteLine("Digite o tamanho da área de ação (X Y -> necessário ter espaço entre as informações):");
+            string a = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite a posição inicial em X para o robô 1, a posição inicial em Y e a direção inicial (X Y Z -> necessidade de espaços):");
+            string b = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite a linha de comando para movimento do robô 1 (sem espaçamento entre as informações):");
+            string c = Console.ReadLine().ToUpper();
+
+            Robot r = new Robot(a, b, c);
+
+            r.DefineDirections();
+            r.Output();
+
+
+            Console.WriteLine("Digite a posição inicial em X (robô 2), a posição inicial em Y e a direção inicial (X Y Z -> necessidade de espaços):");
+            string e = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite a linha de comando para movimento do robô 2 (sem espaçamento entre as informações):");
+            string f = Console.ReadLine().ToUpper();
+
+            Robot r2 = new Robot(a, e, f);
+
+            r2.DefineDirections();
+            r2.Output();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/repos/RoboApp/RoboApp.ConsoleApp/Robot.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboApp.ConsoleApp
+{
+    using System;
+    public class Robot
+    {
+
+        private char[] userInput;
+        private int[] vetorx;
+        private int[] vetory;
+        private char[] direction;
+        private char IniDirection;
+        private int local;
+        private int posx;
+        private int posy;
+        private string[] vet;
+        private string[] vet2;
+        public Robot(string b, string d, string a)
+        {
+            this.userInput = a.ToCharArray();
+            this.vet2 = b.Split(' ');
+            this.vetorx = new int[int.Parse(vet2[0])];
+            this.vetory = new int[int.Parse(vet2[1])];
+            this.vet = d.Split();
+            this.posx = int.Parse(vet[0]);
+            this.posy = int.Parse(vet[1]);
+            this.IniDirection = char.Parse(vet[2]);
+        }
+
+        public void DefineDirections()
+        {
+            this.direction = "NLSO".ToCharArray();
+            this.local = "NLSO".IndexOf(IniDirection);
+        }
+        
+        public void Output()
+        {
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == 'E' || userInput[i] == 'e')
+                {
+                    if (local!=0) {
+                        this.local = local - 1;
+                    }
+                    else if (local == 0)
+                    {
+                        this.local = local + 3;
+                    }
+                }
+
+                if (userInput[i] == 'D' || userInput[i] == 'd')
+                {
+                    if (local != 3)
+                    {
+                        this.local = local + 1;
+                    }
+                    else if (local == 3)
+                    {
+                        this.local = local - 3;
+                    }
+                }
+
+                if (userInput[i] == 'M' || userInput[i] == 'm')
+                {
+                    if (local == 0)
+                    {
+                        posy++;
+                    }
+                    else if (local == 1)
+                    {
+                        posx++;
+                    }
+                    else if (local == 2)
+                    {
+                        posy--;
+                    }
+                    else if (local == 3)
+                    {
+                        posx--;
+                    }
+                }
+            }
+
+           
+
+            if (posx <= vetorx.Length)
+            {
+                Console.Write(posx + " ");
+            }
+            else {
+                Console.WriteLine(" ");
+                Console.WriteLine("Erro, posição final em X fora da área registrada.");
+            }
+
+            if (posy <= vetory.Length)
+            {
+                Console.Write(posy + " ");
+            }
+            else
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Erro: a posição final em Y está fora da área registrada.");
+            }
+
+            Console.WriteLine(direction[local]);
+        }
+
+    }
+}
